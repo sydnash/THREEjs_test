b6abe236bcb7859741ebce5ce126967b94dfd65e
sydnash <157621271@qq.com>
2017-05-10 14:38:55 +0800

    use scrollwidth and height. make double side see.

--- js/va3c.js
+++ js/va3c.js
@@ -8,7 +8,7 @@
         }
         this.domElement = domElement
         this.renderner = new THREE.WebGLRenderer({alpha:1, antialias: true, clearColor: 0x0});
-        this.renderner.setSize(window.innerWidth, window.innerHeight)
+        this.renderner.setSize(domElement.scrollWidth, domElement.scrollHeight);
         this.renderner.shadowMap = true;
         this.domElement.appendChild(this.renderner.domElement);
 
@@ -16,11 +16,13 @@
         this.model = new THREE.Object3D();
 
         this.camera = new THREE.PerspectiveCamera(45, this.domElement.clientWidth / this.domElement.clientHeight, 1, 100000);
-        this.camera.position.set(5000, 5000, 5000);
+        this.camera.position.set(0, 0, 5000);
 
         this.controls = new THREE.FirstPersonControls(this.camera, this.renderner.domElement);
 
-        this.cameraLight = new THREE.PointLight(0xffffff, 1, 0);
+        this.cameraLight = new THREE.PointLight(0xffffff, 0.1, 0);
+        this.scene.add(this.cameraLight);
+
         this.scene.add(this.camera);
         this.camera.lookAt(new THREE.Vector3(0, 0, 0));
         this.scene.add(this.model);
@@ -51,6 +53,7 @@
             self = this
             loader.load(filename, function (result) {
                 self.model.add(result)
+                self.all2DoubleSize(self.model)
 
                     var geometry = new THREE.BoxGeometry(500, 500, 500)
                     var material = new THREE.MeshBasicMaterial({color : 0x00ff00 });
@@ -85,17 +88,30 @@
             var pos = convertPosition(  43, -75, 10000 );
             light.position = pos;
             light.castShadow = true;
-            light.shadowMapWidth = 2048;
-            light.shadowMapHeight = 2048;
+            light.shadow.mapSize.width = 2048;
+            light.shadow.mapSize.height = 2048;
             var d = 10000;
-            light.shadowCameraLeft = -d;
-            light.shadowCameraRight = d;
-            light.shadowCameraTop = d * 2;
-            light.shadowCameraBottom = -d * 2;
+            light.shadow.camera.left = -d;
+            light.shadow.camera.right = d;
+            light.shadow.camera.top = d * 2;
+            light.shadow.camera.bottom = -d * 2;
 
-            light.shadowCameraNear = 1000;
-            light.shadowCameraFar = 20000;
+            light.shadow.camera.near = 1;
+            light.shadow.camera.far = 20000;
             this.scene.add( this.light );
+        },
+        all2DoubleSize : function(obj) {
+            if (obj != undefined && obj.hasOwnProperty("material")) {
+                obj.material.side = THREE.DoubleSide
+            }
+            for (var i = 0; i < obj.children.length; i++ ) {
+                try {
+                    var m = obj.children[i];
+                    this.all2DoubleSize(m)
+                } catch (e) {
+                    console.log("va3c zoom extents error in mesh loop: " + e);
+                }
+            }
         },
 }
 
